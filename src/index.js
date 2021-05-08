ae4418f6ac4dc1d6e0e3f6faa4fbc9e1efd74180
hazel <liam.k.becker@gmail.com>
2021-05-08 18:47:54 -0400

    Testing

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import {Component} from "react/cjs/react.production.min";
 //import App from './App';
 
 class App extends Component {
 
-    state = {};
-
-    componentDidMount() {
-        setInterval(this.hello, 250);
+    constructor(props) {
+        super(props);
+        this.state = {message: ">"};
     }
 
-    hello = () => {
-        fetch('/')
-            .then(response => response.text())
-            .then(message => {
-                this.setState({message: message});
-            });
-    };
+    activateLasers() {
+        this.setState({
+            message: ">====="
+        });
+    }
 
     render() {
         return (
-            <div className="App">
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+            <div>
+                <button onClick={this.activateLasers}>Activate Lasers</button>
+                <h2>{this.state.message}</h2>
             </div>
         );
     }
 }
 
+ReactDOM.render(<App />, document.getElementById('root'));
