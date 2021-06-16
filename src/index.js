056c9730158ed31ec2c72bcfbcf5f1063064d60e
hazelbecker <beckerhazel3@gmail.com>
2021-06-16 15:06:58 -0400

    JSX

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import {Component} from "react/cjs/react.production.min";
 //import App from './App';
 
+let thisPlayer = {
+    name: 'Beac'
+}
+
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {message: ">"};
+        this.state = {message: ">", player: thisPlayer};
     }
 
     activateLasers = () => {
@@ -26,7 +30,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <button onClick={this.activateLasers}>Activate Lasers</button>
+                <button onClick={this.activateLasers}>Activate Lasers {this.state.player.name}</button>
                 <button onClick={this.deactivateLasers}>Deactivate Lasers</button>
                 <h2>{this.state.message}</h2>
             </div>
